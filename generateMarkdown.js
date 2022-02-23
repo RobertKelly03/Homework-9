401dc8f131f37360e57d0488a6b1a3f80f9a3a53
Robert Kelly <rk078701@gmail.com>
2022-02-22 18:58:45 -0800

    adding first inputs for readme generator

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,33 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-//function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-//function renderLicenseLink(license) {}
+function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-//function renderLicenseSection(license) {}
-  //return license.prompt()
+function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-//function generateReadMe(data) {
-  //return `# ${data.title}
+function generateMarkdown(data) {
+  return `# ${data.title}
 
-//`;
-//}
+`;
+}
 
-//module.exports = generateMarkdown;
-
-
-// generateReadme function populating the README.md
-function generateReadme(answers) {
-  return `
-<h1 align="center">${answers.projectTitle} 👋</h1>
-## License
-![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
-<br />
-This application is covered by the ${answers.license} license. 
-## Contributing
- ${answers.contributing}
+module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,61 +1,33 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 const fs = require('fs');
-const genereateMarkdown = require('./utils/generateMarkdown.js');
-const { type } = require('os');
-console.log("Welcome to my README generator")
-console.log("Answer the following questions to generate a high quality README for your project")
+const util = require("util");
 
-const questions = []
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of the project?',
-        validate: your_Input +> {
-            if (your_Input) {
-                return true;
-            } else {
-                console.log('Enter a title to continue!');
-                return false;
-            }
-
-        }
-
-    },
-
-    {
-        type: 'checkbox',
-        name: 'licence',
-        message: 'Choose a license that will best suit your project.',
-        choices: ['MPL 2.0', 'GNU', 'Apache ', 'MIT', 'None of the above'],
-        validate: your_licence => {
-            if (your_licence) {
-                return true;
-            } else {
-                console.log('Select a licence for the project.');
-                return false;
-            }
-        }
-    }
-
-    {
-        type: 'input',
-        name: 'contribution'
-    }
+const generateMarkdown = require('./util/generateMarkdown');
 
 
 
-
-
-
-
-
-
-    function writeToFile(fileName, data) {
-        fs.writeFile(fileName, data, (err) => {
-            if (err) {
-                return console.log(err);
-
-            }
-            console.log("success ")
-        })
-    }
+const questions = [{
+    type: "input",
+    message: "What is the title of the project?",
+    name: "Title"
+}, {
+    type: "input",
+    message: "What is the title of the project?",
+    name: "Title"
+}, {
+    type: "input",
+    message: "What is the title of the project?",
+    name: "Title"
+},  {
+    type: "input",
+    message: "What is the title of the project?",
+    name: "Title"
+},  {
+    type: "input",
+    message: "What is the title of the project?",
+    name: "Title"
+},  {
+    type: "input",
+    message: "What is the title of the project?",
+    name: "Title"
+}]
