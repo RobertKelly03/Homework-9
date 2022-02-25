2a9e31f40b3d352ac3bd5be5e7ed5fa2c64a7a01
Robert Kelly <rk078701@gmail.com>
2022-02-24 19:05:09 -0800

    Testing the read me and working out problems with altering the original readme

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
-const generateReadme = require("./generateMarkdown")
+const generateReadme = require("./generateMarkdown.js")
 const writeFileAsync = util.promisify(fs.writeFile);
 
 //Prompt the user questions to populate the README.md
